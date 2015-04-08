7320d6051c4546b972414dc71ee7b734f9777ae3
Julian Doherty <madlep@madlep.com>
2015-04-08 10:34:28 +1000

    set up spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+require 'rack/test'
+require 'rack-ecg'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.disable_monkey_patching!
+
+  config.warnings = true
+
+  config.profile_examples = 10
+
+  config.include Rack::Test::Methods
+end
