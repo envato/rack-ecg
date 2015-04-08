8873129ed128394f283b9938e47c4c2f30a3ca02
Julian Doherty <madlep@madlep.com>
2015-04-08 10:55:52 +1000

    allow mounted_path to be set

--- lib/rack-ecg.rb
+++ lib/rack-ecg.rb
@@ -2,9 +2,13 @@ require "rack-ecg/version"
 
 module Rack
   class ECG
+    DEFAULT_MOUNTED_PATH = "/_ecg"
+
+    attr_reader :mounted_path
+
     def initialize(app, options={})
       @app = app
-      @options = options
+      @mounted_path = options.delete(:mounted_path) || DEFAULT_MOUNTED_PATH
     end
 
     def call(env)
@@ -14,11 +18,6 @@ module Rack
       else
         @app.call(env)
       end
-    end
-
-    private
-    def mounted_path
-      "/_ecg"
     end
   end
 end
--- spec/rack_middleware_spec.rb
+++ spec/rack_middleware_spec.rb
@@ -3,7 +3,13 @@ RSpec.describe "when used as middleware" do
     opts = options
     Rack::Builder.new do
       use Rack::ECG, opts
-      run -> (env) { [200, {}, ["Hello, World"]] }
+      run -> (env) {
+        if env["PATH_INFO"] == "/hello/world"
+          [200, {}, ["Hello, World"]]
+        else
+          [404, {}, ["Goodbye, World"]]
+        end
+      }
     end
   }
   let(:options) {
@@ -12,18 +18,18 @@ RSpec.describe "when used as middleware" do
 
   context "main app" do
     it "responds OK for normal requests" do
-      get '/'
+      get "/hello/world"
       expect(last_response).to be_ok
     end
 
     it "doesn't include an X-Rack-ECG-Version custom header" do
-      get "/foo/bar"
+      get "/hello/world"
       expect(last_response.header["X-Rack-ECG-Version"]).to be_nil
     end
   end
 
   context "ecg app" do
-    it "responds OK from the /_ecg url" do
+    it "responds " do
       get "/_ecg"
       expect(last_response).to be_ok
     end
@@ -31,6 +37,17 @@ RSpec.describe "when used as middleware" do
     it "includes an X-Rack-ECG-Version custom header" do
       get "/_ecg"
       expect(last_response.header["X-Rack-ECG-Version"]).to eq(Rack::ECG::VERSION)
+    end
+
+    context "when mounted_path is set" do
+      let(:options) {
+        {mounted_path: "/health_check"}
+      }
+
+      it "responds from that path" do
+        get "/health_check"
+        expect(last_response.header["X-Rack-ECG-Version"]).to eq(Rack::ECG::VERSION)
+      end
     end
   end
 end
