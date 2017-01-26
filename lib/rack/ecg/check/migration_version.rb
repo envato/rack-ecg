d6c7308d5aeb6eae095bbe10b195288f1e4eba50
Eaden McKee <_@eaden.net>
2017-01-26 14:18:28 +0000

    check if migrations are up to date

--- lib/rack/ecg/check/migration_version.rb
+++ lib/rack/ecg/check/migration_version.rb
@@ -9,6 +9,12 @@ module Rack
             if defined?(ActiveRecord)
               connection = ActiveRecord::Base.connection
               value = connection.select_value("select max(version) from schema_migrations")
+              # Assuming you're using Rails if you're using ActiveRecord Migrations
+              max_migration = Dir.glob(Rails.root.to_s + "/db/migrate/*.rb").map{|file| file.split('/').last.split('_').first.to_i }.max
+              if value.to_s != max_migration.to_s
+                status = false
+                value = "Migrations not up to date. Max: #{max_migration} Current: #{value}"
+              end
             else
               status = false
               value = "ActiveRecord not found"
